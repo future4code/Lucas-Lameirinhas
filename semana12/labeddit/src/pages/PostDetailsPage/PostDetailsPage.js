d5eeb3b21b641443f3e2ccc3b2d1a781dbcba044
Luclamei16 <luclamei16@gmail.com>
2021-08-30 13:02:14 -0300

    comecei o projeto labeddit e ja realizei o router dele com coordinators

---
+++ semana12/labeddit/src/pages/PostDetailsPage/PostDetailsPage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const PostDetailsPage = () => {
+    return (
+        <div>
+            <h1>Post Details Page</h1>
+        </div>
+    )
+}
+
+export default PostDetailsPage
